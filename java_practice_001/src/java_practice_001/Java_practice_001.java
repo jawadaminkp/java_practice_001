6a2620a26001e848cde759514a7425450079cdb0
jawad@jawad.com <59015763+jawadaminkp@users.noreply.github.com>
2021-01-01 22:04:37 +0500

    practice program no 1

---
+++ java_practice_001/src/java_practice_001/Java_practice_001.java
@@ -0,0 +1,15 @@
+package java_practice_001;
+
+
+public class Java_practice_001 {
+
+    public static void main(String[] args) {
+      
+        
+        System.out.println("Hello World");
+        System.out.println("Happy New Year");
+        System.out.println("Jawad Amin here");
+        
+    }
+    
+}
